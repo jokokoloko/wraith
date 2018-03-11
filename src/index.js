dbf12ece98c564e7678678fe56ef890c8c8007eb
jokokoloko <jhowland82@gmail.com>
2018-03-11 13:42:49 -0700

    Changed 'registerServiceWorker' to 'service'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import service from './service';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+service();
