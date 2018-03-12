672bf81fc336aec1473506d5a7066f3c4d2208f5
jokokoloko <jhowland82@gmail.com>
2018-03-11 17:00:06 -0700

    Modified folder structure and paths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import service from './service';
-import App from './App';
+import service from './js/service';
+import App from './js/react/App';
+import './css/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 service();
