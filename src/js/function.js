f78fd2e6c58481059703000ee454a5deabc2d329
jokokoloko <jhowland82@gmail.com>
2018-03-23 04:38:43 -0700

    Applied new prettier format to all files

--- src/js/function.js
+++ src/js/function.js
@@ -1,57 +1,57 @@
 export function capitalize(text) {
-	return text.replace(/\b\w/g, l => l.toUpperCase());
+    return text.replace(/\b\w/g, (l) => l.toUpperCase());
 }
 export function textify(text) {
-	return (
-		text
-			.toString()
-			.toLowerCase()
-			// Replace - with spaces
-			.replace(/-/g, ' ')
-			// Remove all non-word chars
-			.replace(/[^\w\-]+/g, ' ')
-			// Trim spaces from start and end of text
-			.trim()
-	);
+    return (
+        text
+            .toString()
+            .toLowerCase()
+            // Replace - with spaces
+            .replace(/-/g, ' ')
+            // Remove all non-word chars
+            .replace(/[^\w\-]+/g, ' ')
+            // Trim spaces from start and end of text
+            .trim()
+    );
 }
 export function classify(text) {
-	return (
-		text
-			.toString()
-			.toLowerCase()
-			// Remove all non-word chars
-			.replace(/[^\w\-]+/g, ' ')
-			// Trim spaces from start and end of text
-			.trim()
-	);
+    return (
+        text
+            .toString()
+            .toLowerCase()
+            // Remove all non-word chars
+            .replace(/[^\w\-]+/g, ' ')
+            // Trim spaces from start and end of text
+            .trim()
+    );
 }
 export function slugify(text) {
-	const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;';
-	const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------';
-	const p = new RegExp(a.split('').join('|'), 'g');
-	return (
-		text
-			.toString()
-			.toLowerCase()
-			// Replace spaces with -
-			.replace(/\s+/g, '-')
-			// Replace special chars
-			.replace(p, c => b.charAt(a.indexOf(c)))
-			// Replace & with 'and'
-			.replace(/&/g, '-and-')
-			// Remove all non-word chars
-			.replace(/[^\w\-]+/g, '')
-			// Replace multiple - with single -
-			.replace(/\-\-+/g, '-')
-			// Replace something with single -
-			.replace(/[\s_-]+/g, '-')
-			// Trim - from start of text
-			.replace(/^-+/, '')
-			// Trim - from end of text
-			.replace(/-+$/, '')
-			// Trim spaces from start and end of text
-			.trim()
-	);
+    const a = 'àáäâèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;';
+    const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------';
+    const p = new RegExp(a.split('').join('|'), 'g');
+    return (
+        text
+            .toString()
+            .toLowerCase()
+            // Replace spaces with -
+            .replace(/\s+/g, '-')
+            // Replace special chars
+            .replace(p, (c) => b.charAt(a.indexOf(c)))
+            // Replace & with 'and'
+            .replace(/&/g, '-and-')
+            // Remove all non-word chars
+            .replace(/[^\w\-]+/g, '')
+            // Replace multiple - with single -
+            .replace(/\-\-+/g, '-')
+            // Replace something with single -
+            .replace(/[\s_-]+/g, '-')
+            // Trim - from start of text
+            .replace(/^-+/, '')
+            // Trim - from end of text
+            .replace(/-+$/, '')
+            // Trim spaces from start and end of text
+            .trim()
+    );
 }
 // // Domainify
 // export function domainify($string) {
