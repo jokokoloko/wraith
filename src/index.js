6b89c50266aadf4a1152b5563092debc29bb9e94
jokokoloko <jhowland82@gmail.com>
2018-03-22 21:16:33 -0700

    Updated styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import service from './js/service';
 import App from './js/react/App';
-import './css/index.css';
+import './css/theme.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 service();
