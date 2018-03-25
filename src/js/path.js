774c09a4ee4c849d9ae865b7fb37ff99fb7bd0e7
jokokoloko <jhowland82@gmail.com>
2018-03-24 19:59:08 -0700

    Renamed route.js to path.js and modified const names

---
+++ src/js/path.js
@@ -0,0 +1,10 @@
+// Private
+export const _Home = '/dashboard';
+
+// Public
+export const Login = '/login';
+export const Register = '/register';
+export const About = '/about';
+export const Post = '/post';
+export const PostView = '/:id';
+export const Home = '/';
