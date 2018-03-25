83ad453c4b95fb1f804e3c2f93dd442bfabf3e51
jokokoloko <jhowland82@gmail.com>
2018-03-24 17:56:34 -0700

    Created client.js and route.js
    Files for constants.

---
+++ src/js/client.js
@@ -0,0 +1,2 @@
+// Identity
+export const BRAND = 'Spectre';
---
+++ src/js/route.js
@@ -0,0 +1,10 @@
+// Private
+export const DASHBOARD = '/dashboard';
+
+// Public
+export const LOGIN = '/login';
+export const REGISTER = '/register';
+export const ABOUT = '/about';
+export const POST = '/post';
+export const POSTVIEW = '/:id';
+export const HOME = '/';
