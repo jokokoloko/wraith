c6b33c863681a1368548d4041138b27e2e403e7b
jokokoloko <jhowland82@gmail.com>
2018-03-26 19:52:48 -0700

    Added Image

---
+++ src/js/react/unit/Image.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loader from './Loader';
+
+class Image extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+            error: false,
+        };
+    }
+    componentDidMount() {
+        const image = document.createElement('img');
+        image.src = this.props.source;
+        image.onload = () => {
+            this.setState({
+                loaded: true,
+                error: false,
+            });
+            if (this.props.onLoad) {
+                this.props.onLoad(image);
+            }
+        };
+        image.onerror = (error) => {
+            this.setState({
+                loaded: false,
+                error: true,
+            });
+            if (this.props.onError) {
+                this.props.onError(error);
+            }
+        };
+    }
+    render() {
+        const { loaded, error } = this.state;
+        const { position, source, alternate } = this.props;
+        return loaded === false ? <Loader position="exact-center" error={error} /> : <img className={`img-fluid ${position} unit`} src={source} alt={alternate} />;
+    }
+}
+
+Image.propTypes = {
+    position: PropTypes.string,
+    source: PropTypes.string,
+    alternate: PropTypes.string,
+    onLoad: PropTypes.func,
+    onError: PropTypes.func,
+};
+
+Image.defaultProps = {
+    position: 'no-position',
+    source: undefined,
+    alternate: undefined,
+    onLoad: undefined,
+    onError: undefined,
+};
+
+export default Image;
