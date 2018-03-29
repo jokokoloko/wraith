4f307a9ed7dc9a68af899a0e22512117d8514fa8
jokokoloko <jhowland82@gmail.com>
2018-03-28 19:12:07 -0700

    Minor edit to _Private.js

--- src/js/react/_Private.js
+++ src/js/react/_Private.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-import _Empty from './_404';
 import _Home from './_Home';
+import _Empty from './_404';
 
 const _Private = ({ match }) => (
     <Fragment>
@@ -10,7 +10,7 @@ const _Private = ({ match }) => (
 
         <Switch>
             <Route path={`${match.path}/post`} component={_Home} />
-            <Route path={`${match.path}`} component={_Home} />
+            <Route path={`${match.path}`} component={_Home} exact />
             <Route component={_Empty} />
         </Switch>
     </Fragment>
