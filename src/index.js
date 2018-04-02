6e4cad8a55132da21a587fc2d00b5c95409c38e3
jokokoloko <jhowland82@gmail.com>
2018-04-01 23:05:23 -0700

    Modified index.js for Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import service from './service';
 import Root from './js/react/Root';
+import configureStore from './js/redux/store';
+import { usersLoad } from './js/redux/action/actionUser';
+import { postsLoad } from './js/redux/action/actionPost';
+import { reviewsLoad } from './js/redux/action/actionReview';
 import './css/theme.css';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const store = configureStore();
+store.dispatch(usersLoad());
+store.dispatch(postsLoad());
+store.dispatch(reviewsLoad());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Root />
+    </Provider>,
+    document.getElementById('root'),
+);
 service();
