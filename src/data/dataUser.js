82e483a90c3581cb269f705a6241fa70a59cb562
jokokoloko <jhowland82@gmail.com>
2018-04-01 21:50:59 -0700

    Added /data with mock data files

---
+++ src/data/dataUser.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        key: 'Hi26sctReYUfswWS2nvF7BsmQGf2',
+        email: 'jhowland82@gmail.com',
+        role: 'administrator',
+        id: 'doctor-zoidberg',
+        nameFirst: 'Doctor',
+        nameLast: 'Zoidberg',
+        address1: '350 West Ash Street',
+        address2: 'Unit 1103',
+        city: 'San Diego',
+        state: 'California',
+        country: 'USA',
+        zip: 92101,
+        avatar: 'http://cede0.com/image/cover.jpg',
+    },
+    {
+        key: 'vQH8jkIBnYcKxUu64fLAbKIHm2Z2',
+        email: 'jhowland82@yahoo.com',
+        role: 'administrator',
+        id: 'joy-division',
+        nameFirst: 'Joy',
+        nameLast: 'Division',
+        address1: '11568 Cohansey Road',
+        address2: undefined,
+        city: 'San Diego',
+        state: 'California',
+        country: 'USA',
+        zip: 92131,
+        avatar: 'http://cede0.com/image/unknown-pleasures.jpg',
+    },
+];
+
+export default users;
