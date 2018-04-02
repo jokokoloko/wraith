650ff314766ebdf8d779754d44e2a14ddcfc2563
jokokoloko <jhowland82@gmail.com>
2018-04-01 21:43:42 -0700

    Moved fakeAuth back into Root.js

--- src/js/react/Root.js
+++ src/js/react/Root.js
@@ -10,7 +10,18 @@ import Empty from './404';
 import Compass from './region/Compass';
 import Header from './region/Header';
 import Footer from './region/Footer';
-import fakeAuth from '../../api/fakeAuth';
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        this.isAuthenticated = false;
+        setTimeout(cb, 100); // fake async
+    },
+};
 
 const AuthButton = withRouter(
     ({ history }) =>
