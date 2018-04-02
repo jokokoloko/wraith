408c34df9585b04c096657cb4d4075592136cdc6
jokokoloko <jhowland82@gmail.com>
2018-04-01 19:58:21 -0700

    Modified About.js

--- src/js/react/About.js
+++ src/js/react/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Basic from './section/Basic';
 
 const About = () => (
     <main id="main" role="main">
         <div className="container-fluid">
-            <h1>About</h1>
+            <Basic space="space-xs-50 space-lg-80">
+                <header className="text-center">
+                    <h1>About</h1>
+                </header>
+            </Basic>
         </div>
     </main>
 );
