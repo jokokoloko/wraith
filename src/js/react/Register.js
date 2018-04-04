dc1db37ad576249ef99f94de193f51c4e916144d
jokokoloko <jhowland82@gmail.com>
2018-04-04 05:15:03 -0700

    Modified Register.js

--- src/js/react/Register.js
+++ src/js/react/Register.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Basic from './section/Basic';
+import FormRegister from './form/FormRegister';
 
 const Register = () => (
     <main id="main" role="main">
         <div className="container-fluid">
-            <h1>Register</h1>
+            <Basic space="space-xs-50 space-lg-80">
+                <header className="text-center">
+                    <h1>Register</h1>
+                </header>
+
+                <section>
+                    <FormRegister />
+                </section>
+            </Basic>
         </div>
     </main>
 );
