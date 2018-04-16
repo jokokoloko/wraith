2d23c53810398a10dc7f5f67b6c0a524a71d7c6f
jokokoloko <jhowland82@gmail.com>
2018-04-15 19:11:44 -0700

    Added Account.js

---
+++ src/js/react/widget/Account.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as path from '../../path';
+
+const Account = ({ authenticated, onLogOut }) => {
+    const fieldClass = 'btn btn-lg';
+    const wrapperClass = 'account ml-auto';
+    return authenticated === true ? (
+        <div className={`member ${wrapperClass}`}>
+            <Link className={`to-dashboard ${fieldClass} btn-secondary`} to={path._Private}>
+                @
+            </Link>
+            <button type="button" className={`on-logout ${fieldClass} btn-danger`} onClick={onLogOut} role="menuitem" tabIndex="-1">
+                Log Out
+            </button>
+        </div>
+    ) : (
+        <div className={`guest ${wrapperClass}`}>
+            <Link className={`to-register ${fieldClass} btn-success`} to={path.Register}>
+                Register
+            </Link>
+            <Link className={`to-login ${fieldClass} btn-primary`} to={path.Login}>
+                Log In
+            </Link>
+        </div>
+    );
+};
+
+Account.propTypes = {
+    authenticated: PropTypes.bool.isRequired,
+    // profile: PropTypes.objectOf(PropTypes.any).isRequired,
+    onLogOut: PropTypes.func.isRequired,
+};
+
+export default Account;
