e11f8fd15048ea0d2e04d8c07d6f2f069bcb889b
jokokoloko <jhowland82@gmail.com>
2018-04-16 22:42:01 -0700

    Added Toolbar.js

---
+++ src/js/react/region/Toolbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as client from '../../client';
+import * as path from '../../path';
+
+const Toolbar = ({ match }) => (
+    <aside id="toolbar">
+        <Link className="toolbar-brand" title={client.BRAND} rel="home" to={path.Root}>
+            {client.BRAND}
+        </Link>
+        <ul className="toolbar-nav nav flex-column">
+            <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to={`${match.path}`}>
+                    Dashboard
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to={`${match.path}${path._Post}`}>
+                    Posts
+                </NavLink>
+            </li>
+        </ul>
+    </aside>
+);
+
+Toolbar.propTypes = {
+    match: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default Toolbar;
