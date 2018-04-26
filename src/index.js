ae2a4622f2c53cf91a509eb191c2c75714bc2003
jokokoloko <jhowland82@gmail.com>
2018-04-25 23:40:03 -0700

    Removed multiple api and action files

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import { Provider } from 'react-redux';
 import service from './service';
 import Root from './js/react/Root';
 import configureStore from './js/redux/store';
-import { usersLoad } from './js/redux/action/actionUser';
-import { postsLoad } from './js/redux/action/actionPost';
-import { reviewsLoad } from './js/redux/action/actionReview';
 import '../node_modules/toastr/build/toastr.min.css'; // 6 kb
 import './css/theme.css';
 
 const store = configureStore();
-store.dispatch(usersLoad());
-store.dispatch(postsLoad());
-store.dispatch(reviewsLoad());
 
 ReactDOM.render(
     <Provider store={store}>
