0422b7ada623753fbb3a3b67579af61327ca7b2c
jokokoloko <jhowland82@gmail.com>
2018-05-02 00:31:21 -0700

    Added users list to _UserHome.js and Team.js

--- src/js/react/Team.js
+++ src/js/react/Team.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Basic from './section/Basic';
+import Loader from './unit/Loader';
 
-const Team = () => (
+const Team = ({ loading, users }) => (
     <main id="main" role="main">
         <div className="container-fluid">
             <Basic space="space-xs-50 space-lg-80">
@@ -9,8 +12,39 @@ const Team = () => (
                     <h1>Team</h1>
                 </header>
             </Basic>
+
+            <Basic space="space-xs-50 space-lg-80">
+                <section className="text-center">
+                    {loading ? (
+                        <Loader position="exact-center fixed" label="Loading users" />
+                    ) : users.length > 0 ? (
+                        <ul className="list-unstyled">
+                            {users.map((user, index) => (
+                                <li key={user.id} className={`item-${index + 1}`}>
+                                    {user.id}
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p className="empty">No users.</p>
+                    )}
+                </section>
+            </Basic>
         </div>
     </main>
 );
 
-export default Team;
+Team.propTypes = {
+    match: PropTypes.objectOf(PropTypes.any).isRequired,
+    loading: PropTypes.bool.isRequired,
+    users: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+function mapStateToProps({ call, users }) {
+    return {
+        loading: call > 0,
+        users,
+    };
+}
+
+export default connect(mapStateToProps)(Team);
--- src/js/react/_UserHome.js
+++ src/js/react/_UserHome.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Basic from './section/Basic';
+import Loader from './unit/Loader';
 
-const _UserHome = () => (
+const _UserHome = ({ loading, users }) => (
     <main id="main" role="main">
         <div className="container-fluid">
             <Basic container="container-fluid" space="space-xs-50 space-lg-80">
@@ -9,8 +12,39 @@ const _UserHome = () => (
                     <h1>Users</h1>
                 </header>
             </Basic>
+
+            <Basic container="container-fluid" space="space-xs-50 space-lg-80">
+                <section>
+                    {loading ? (
+                        <Loader position="exact-center fixed" label="Loading users" />
+                    ) : users.length > 0 ? (
+                        <ul className="list-unstyled">
+                            {users.map((user, index) => (
+                                <li key={user.id} className={`item-${index + 1}`}>
+                                    {user.id}
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p className="empty">No users.</p>
+                    )}
+                </section>
+            </Basic>
         </div>
     </main>
 );
 
-export default _UserHome;
+_UserHome.propTypes = {
+    match: PropTypes.objectOf(PropTypes.any).isRequired,
+    loading: PropTypes.bool.isRequired,
+    users: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+function mapStateToProps({ call, users }) {
+    return {
+        loading: call > 0,
+        users,
+    };
+}
+
+export default connect(mapStateToProps)(_UserHome);
