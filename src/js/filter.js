6364672040c203d1d7c343837e0edb49f684cb74
jokokoloko <jhowland82@gmail.com>
2018-05-04 21:54:00 -0700

    Added a quick comment in filter.js

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,6 +1,6 @@
 // findByProperty - scans array until it finds an exact match and then stops, ignoring any remaining objects in the array
 export const findByProperty = (array, name, value) => {
-    const object = array.find((object) => object[name] === value);
+    const object = array.find((object) => object[name] === value); // returns undefined if no match
     return object ? object : {}; // returns only one object from the array or an empty object if no match (possibly remove this line and refactor)
 };
 
