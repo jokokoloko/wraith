504c7931ccf487ebdca22f8568d5ccaff72dfcf3
jokokoloko <jhowland82@gmail.com>
2018-05-04 21:26:51 -0700

    Heavily refactored filter.js

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,53 +1,11 @@
-// ID
-export const filterByID = (array, id) => {
-    const objects = array.filter((object) => object.id === id); // scans whole array and can return multiple objects
-    if (objects.length > 0) return objects; // returns a new array containing all the matching objects
-    return []; // returns an empty array if no matches
+// filterByProperty - returns a new array containing all the matching objects or an empty array if no matches
+export const filterByProperty = (array, name, value) => array.filter((object) => object[name] === value);
+
+// findByProperty - scans array until it finds an exact match and then stops, ignoring any remaining objects in the array
+export const findByProperty = (array, name, value) => {
+    const object = array.find((object) => object[name] === value);
+    return object ? object : {}; // returns only one object from the array or an empty object if no match (possibly remove this line and refactor)
 };
 
-export const findByID = (array, id) => {
-    const object = array.find((object) => object.id === id); // scans array until it finds an exact match and then stops, ignoring any remaining objects in the array
-    if (object) return object; // only returns one object
-    return {}; // returns an empty object if no match
-};
-
-export const findIndexByID = (array, id) => {
-    const index = array.findIndex((object) => object.id === id);
-    return index; // returns the index number of the object within the array or -1 if no match
-};
-
-// UID
-export const filterByUID = (array, uid) => {
-    const objects = array.filter((object) => object.uid === uid); // scans whole array and can return multiple objects
-    if (objects.length > 0) return objects; // returns a new array containing all the matching objects
-    return []; // returns an empty array if no matches
-};
-
-export const findByUID = (array, uid) => {
-    const object = array.find((object) => object.uid === uid); // scans array until it finds an exact match and then stops, ignoring any remaining objects in the array
-    if (object) return object; // only returns one object
-    return {}; // returns an empty object if no match
-};
-
-export const findIndexByUID = (array, uid) => {
-    const index = array.findIndex((object) => object.uid === uid);
-    return index; // returns the index number of the object within the array or -1 if no match
-};
-
-// Slug
-export const filterBySlug = (array, slug) => {
-    const objects = array.filter((object) => object.slug === slug); // scans whole array and can return multiple objects
-    if (objects.length > 0) return objects; // returns a new array containing all the matching objects
-    return []; // returns an empty array if no matches
-};
-
-export const findBySlug = (array, slug) => {
-    const object = array.find((object) => object.slug === slug); // scans array until it finds an exact match and then stops, ignoring any remaining objects in the array
-    if (object) return object; // only returns one object
-    return {}; // returns an empty object if no match
-};
-
-export const findIndexBySlug = (array, slug) => {
-    const index = array.findIndex((object) => object.slug === slug);
-    return index; // returns the index number of the object within the array or -1 if no match
-};
+// findIndexByProperty - returns the index number of the object within the array or -1 if no match
+export const findIndexByProperty = (array, name, value) => array.findIndex((object) => object[name] === value);
