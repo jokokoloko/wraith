64d8cf8d63e4d87f9a1237b259131b7fdd3e7634
jokokoloko <jhowland82@gmail.com>
2018-05-04 21:51:49 -0700

    Added excludeByProperty to filter.js
    Also renamed filterByProperty to includeByProperty.

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,6 +1,3 @@
-// filterByProperty - returns a new array containing all the matching objects or an empty array if no matches
-export const filterByProperty = (array, name, value) => array.filter((object) => object[name] === value);
-
 // findByProperty - scans array until it finds an exact match and then stops, ignoring any remaining objects in the array
 export const findByProperty = (array, name, value) => {
     const object = array.find((object) => object[name] === value);
@@ -9,3 +6,9 @@ export const findByProperty = (array, name, value) => {
 
 // findIndexByProperty - returns the index number of the object within the array or -1 if no match
 export const findIndexByProperty = (array, name, value) => array.findIndex((object) => object[name] === value);
+
+// includeByProperty - returns a new array containing all the matching objects or an empty array if none match the property
+export const includeByProperty = (array, name, value) => array.filter((object) => object[name] === value);
+
+// excludeByProperty - returns a new array containing all the original objects except for any objects matching the property (used to remove an object from an array)
+export const excludeByProperty = (array, name, value) => array.filter((object) => object[name] !== value);
