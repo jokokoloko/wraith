ed22a71b9bd86253e218577945f486366b24b441
jokokoloko <jhowland82@gmail.com>
2018-05-06 00:52:38 -0700

    Refactored Team.js

--- src/js/react/Team.js
+++ src/js/react/Team.js
@@ -2,37 +2,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Basic from './section/Basic';
+import Feed from './section/Feed';
 import Loader from './unit/Loader';
 
-const Team = ({ loading, users }) => (
-    <main id="main" role="main">
-        <div className="container-fluid">
-            <Basic space="space-xs-50 space-lg-80">
-                <header className="text-center">
-                    <h1>Team</h1>
+const Team = ({ loading, users }) => {
+    const item = 'user';
+    const loopUser = users.map((user, index) => {
+        const count = index + 1;
+        return (
+            <article key={user.id} id={user.id} className={`${item} ${item}-${count} col`}>
+                <header>
+                    <h3>{user.id}</h3>
                 </header>
-            </Basic>
+            </article>
+        );
+    });
+    return (
+        <main id="main" role="main">
+            <div className="container-fluid">
+                <Basic space="space-xs-50 space-lg-80">
+                    <header className="text-center">
+                        <h1>Team</h1>
+                    </header>
+                </Basic>
 
-            <Basic space="space-xs-50 space-lg-80">
-                <section className="text-center">
-                    {loading ? (
-                        <Loader position="exact-center fixed" label="Loading users" />
-                    ) : users.length > 0 ? (
-                        <ul className="list-unstyled">
-                            {users.map((user, index) => (
-                                <li key={user.id} className={`item-${index + 1}`}>
-                                    {user.id}
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p className="empty">No users.</p>
-                    )}
-                </section>
-            </Basic>
-        </div>
-    </main>
-);
+                <Feed space="space-xs-50 space-lg-80" item={item}>
+                    <section className="text-center">
+                        {loading ? (
+                            <Loader position="exact-center fixed" label="Loading users" />
+                        ) : users.length > 0 ? (
+                            <div className="row gutter-xs-30 gutter-lg-50">{loopUser}</div>
+                        ) : (
+                            <article className="empty">
+                                <header>
+                                    <h3>{`No ${item}s`}</h3>
+                                </header>
+                            </article>
+                        )}
+                    </section>
+                </Feed>
+            </div>
+        </main>
+    );
+};
 
 Team.propTypes = {
     match: PropTypes.objectOf(PropTypes.any).isRequired,
