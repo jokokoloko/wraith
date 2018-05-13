cb3a9b303c5135dcd5f05e3d221fbec3725d0a19
jokokoloko <jhowland82@gmail.com>
2018-05-13 00:18:49 -0700

    Added CALLS_VOID along with actionCall.js and reducerCall.js

---
+++ src/js/redux/action/actionCall.js
@@ -0,0 +1,6 @@
+import { CALLS_VOID } from '../type';
+
+// Load
+export const callsVoid = () => ({
+    type: CALLS_VOID,
+});
--- src/js/redux/reducer/reducerCall.js
+++ src/js/redux/reducer/reducerCall.js
@@ -1,10 +1,12 @@
 import initial from '../initial';
-import { USERS_LOAD_REQUEST, USERS_LOAD_SUCCESS, USERS_LOAD_FAILURE } from '../type';
+import { CALLS_VOID, USERS_LOAD_REQUEST, USERS_LOAD_SUCCESS, USERS_LOAD_FAILURE } from '../type';
 
 export default function reducerCall(state = initial.calls, action) {
     switch (action.type) {
+        case CALLS_VOID:
+            return initial.calls;
         case USERS_LOAD_REQUEST:
-            return state.concat([USERS_LOAD_REQUEST]);
+            return state.concat(USERS_LOAD_REQUEST);
         case USERS_LOAD_SUCCESS:
         case USERS_LOAD_FAILURE:
             return state.filter((request) => request !== USERS_LOAD_REQUEST);
