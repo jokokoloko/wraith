90b3f501eef304d146a9acbc129f254c6186baff
jokokoloko <jhowland82@gmail.com>
2018-05-30 17:15:24 -0700

    Refactored reducerCall.js again

--- src/js/redux/reducer/reducerCall.js
+++ src/js/redux/reducer/reducerCall.js
@@ -1,17 +1,21 @@
 import initial from '../initial';
 import { REQUEST, SUCCESS, FAILURE, CALLS_VOID } from '../type';
-import { addStatus, removeStatus } from '../../filter';
+import { checkStatus, removeStatus } from '../../filter';
 
 export default function reducerCall(state = initial.calls, action) {
-    let call = removeStatus(action.type);
+    const type = action.type === CALLS_VOID ? CALLS_VOID : checkStatus(action.type);
+    const call = removeStatus(action.type);
 
-    switch (action.type) {
-        case addStatus(call, REQUEST):
+    console.log('Status:', type, call); // remove
+
+    switch (type) {
+        case REQUEST:
             return state.concat(call);
-        case addStatus(call, SUCCESS):
-        case addStatus(call, FAILURE):
+        case SUCCESS:
+        case FAILURE:
             return state.filter((request) => request !== call);
         case CALLS_VOID:
+            console.log('Bingo!'); // remove
             return initial.calls;
         default:
             return state;
