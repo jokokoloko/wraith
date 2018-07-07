8ab9b3dd6674147c3723d0bec39c0a66f3da7603
jokokoloko <jhowland82@gmail.com>
2018-07-06 22:37:54 -0700

    Modified generateID in function.js

--- src/js/function.js
+++ src/js/function.js
@@ -60,12 +60,13 @@ export const arrayToObject = (array, keyField) =>
     );
 
 // ID
-export const generateID = (string) =>
+export const generateID = () =>
     Math.random()
         .toString(36)
-        .substring(2) +
-    new Date().getTime().toString(36) +
-    string;
+        .substring(2, 15) +
+    Math.random()
+        .toString(36)
+        .substring(2, 15);
 
 // Slug
 export const generateSlug = () =>
