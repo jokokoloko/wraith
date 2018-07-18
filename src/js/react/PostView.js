a298c29a004d4f03981297ce772838aa6b2c227f
jokokoloko <jhowland82@gmail.com>
2018-07-17 23:38:00 -0700

    Connected PostView.js to Redux store to load an display "post" data

--- src/js/react/PostView.js
+++ src/js/react/PostView.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+import * as actionView from '../redux/action/actionView';
 import Basic from './section/Basic';
+import Loader from './unit/Loader';
 
-const PostView = () => (
-    <main id="main" role="main">
-        <div className="container-fluid">
-            <Basic space="space-xs-50 space-lg-80">
-                <header className="text-center">
-                    <h1>Post</h1>
-                </header>
-            </Basic>
-        </div>
-    </main>
-);
+class PostView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingView: true,
+        };
+    }
+    componentDidMount() {
+        const { match, actionView } = this.props;
+        actionView.viewLoad(match.params.slug).then(() =>
+            this.setState({
+                loadingView: false,
+            }),
+        );
+    }
+    render() {
+        const { view: post } = this.props;
+        const { loadingView } = this.state;
+        return loadingView ? (
+            <Loader position="exact-center fixed" label="Loading view" />
+        ) : (
+            <main id="main" role="main">
+                <div className="container-fluid">
+                    <Basic space="space-xs-20 space-lg-80">
+                        {post.id ? (
+                            <header className="text-center">
+                                <h1>{post.title}</h1>
+                            </header>
+                        ) : (
+                            <header className="empty text-center">
+                                <h1>404 - Post</h1>
+                            </header>
+                        )}
+                    </Basic>
+                </div>
+            </main>
+        );
+    }
+}
 
-export default PostView;
+PostView.propTypes = {
+    match: PropTypes.objectOf(PropTypes.any).isRequired,
+    view: PropTypes.objectOf(PropTypes.any).isRequired,
+    actionView: PropTypes.objectOf(PropTypes.func).isRequired,
+};
+
+function mapStateToProps({ view }) {
+    return {
+        view,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actionView: bindActionCreators(actionView, dispatch),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(PostView);
