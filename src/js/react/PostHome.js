9819320c58000a339ade07153b7d43331e364d88
jokokoloko <jhowland82@gmail.com>
2018-07-17 20:29:50 -0700

    Refactored PostHome.js and User.js to be smart, container components
    PostHome.js and User.js now both make API calls in componentDidMount() to fetch and load their necessary data from Firestore.

--- src/js/react/PostHome.js
+++ src/js/react/PostHome.js
@@ -1,70 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-import { POSTS_LOAD_REQUEST } from '../redux/type';
-import { findByString, removeStatus } from '../filter';
-import * as path from '../path';
+import * as actionPost from '../redux/action/actionPost';
 import Basic from './section/Basic';
 import Feed from './section/Feed';
 import Loader from './unit/Loader';
 
-const PostHome = ({ loadingPosts, posts }) => {
-    const item = 'post';
-    const loopPost = posts.map((post, index) => {
-        const count = posts.length - index;
-        return (
-            <article key={post.id} id={post.id} className={`${item} ${item}-${count} node-xs-20`}>
-                <header className="card card-panel">
-                    <div className="card-body">
-                        <h3 className="card-headline">{post.title}</h3>
-                        <p>
-                            {post.excerpt}... <Link to={`${path.Post}/${post.slug}`}>Read More &rarr;</Link>
-                        </p>
-                    </div>
-                </header>
-            </article>
+class PostHome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingPosts: true,
+        };
+    }
+    componentDidMount() {
+        const { actionPost } = this.props;
+        actionPost.postsLoad().then(() =>
+            this.setState({
+                loadingPosts: false,
+            }),
         );
-    });
-    return (
-        <main id="main" role="main">
-            <div className="container-fluid">
-                <Basic space="space-xs-30 space-lg-80">
-                    <header className="text-center">
-                        <h1>Posts</h1>
+    }
+    render() {
+        const { match, posts } = this.props;
+        const { loadingPosts } = this.state;
+        const item = 'post';
+        const loopPost = posts.map((post, index) => {
+            const count = posts.length - index;
+            return (
+                <article key={post.id} id={post.id} className={`${item} ${item}-${count} node-xs-20`}>
+                    <header className="card card-panel">
+                        <div className="card-body">
+                            <h3 className="card-headline">{post.title}</h3>
+                            <p>
+                                {post.excerpt}... <Link to={`${match.path}/${post.slug}`}>Read More &rarr;</Link>
+                            </p>
+                        </div>
                     </header>
-                </Basic>
+                </article>
+            );
+        });
+        return (
+            <main id="main" role="main">
+                <div className="container-fluid">
+                    <Basic space="space-xs-30 space-lg-80">
+                        <header className="text-center">
+                            <h1>Posts</h1>
+                        </header>
+                    </Basic>
 
-                <Feed space="space-xs-20 space-lg-80" item={item}>
-                    <section>
-                        {loadingPosts ? (
-                            <Loader position="exact-center fixed" label="Loading posts" />
-                        ) : posts.length > 0 ? (
-                            loopPost
-                        ) : (
-                            <article className="empty text-center">
-                                <header>
-                                    <h3>{`No ${item}s`}</h3>
-                                </header>
-                            </article>
-                        )}
-                    </section>
-                </Feed>
-            </div>
-        </main>
-    );
-};
+                    <Feed space="space-xs-20 space-lg-80" item={item}>
+                        <section>
+                            {loadingPosts ? (
+                                <Loader position="exact-center fixed" label="Loading posts" />
+                            ) : posts.length > 0 ? (
+                                loopPost
+                            ) : (
+                                <article className="empty text-center">
+                                    <header>
+                                        <h3>{`No ${item}s`}</h3>
+                                    </header>
+                                </article>
+                            )}
+                        </section>
+                    </Feed>
+                </div>
+            </main>
+        );
+    }
+}
 
 PostHome.propTypes = {
-    loadingPosts: PropTypes.bool.isRequired,
+    match: PropTypes.objectOf(PropTypes.any).isRequired,
     posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+    actionPost: PropTypes.objectOf(PropTypes.func).isRequired,
 };
 
-function mapStateToProps({ calls, posts }) {
+function mapStateToProps({ posts }) {
     return {
-        loadingPosts: findByString(calls, removeStatus(POSTS_LOAD_REQUEST)),
         posts,
     };
 }
 
-export default connect(mapStateToProps)(PostHome);
+function mapDispatchToProps(dispatch) {
+    return {
+        actionPost: bindActionCreators(actionPost, dispatch),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(PostHome);
--- src/js/react/User.js
+++ src/js/react/User.js
@@ -1,91 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
-import { USERS_LOAD_REQUEST } from '../redux/type';
-import { findByString, removeStatus } from '../filter';
+import * as actionUser from '../redux/action/actionUser';
 import Basic from './section/Basic';
 import Feed from './section/Feed';
 import Avatar from './unit/Avatar';
 import Loader from './unit/Loader';
 
-const User = ({ loadingUsers, users }) => {
-    const item = 'user';
-    const loopUser = users.map((user, index) => {
-        const count = users.length - index;
-        return (
-            <article key={user.id} id={user.id} className={`${item} ${item}-${count} col-lg-3`}>
-                <header className="card card-panel">
-                    <Link className="card-body" to={`/${user.slug}`}>
-                        <Avatar
-                            position="fit exact-center"
-                            source={user.avatar || 'http://via.placeholder.com/800?text=Avatar'}
-                            alternate={
-                                user.name && user.name.first && user.name.last
+class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingUsers: true,
+        };
+    }
+    componentDidMount() {
+        const { actionUser } = this.props;
+        actionUser.usersLoad().then(() =>
+            this.setState({
+                loadingUsers: false,
+            }),
+        );
+    }
+    render() {
+        const { users } = this.props;
+        const { loadingUsers } = this.state;
+        const item = 'user';
+        const loopUser = users.map((user, index) => {
+            const count = users.length - index;
+            return (
+                <article key={user.id} id={user.id} className={`${item} ${item}-${count} col-lg-3`}>
+                    <header className="card card-panel">
+                        <Link className="card-body" to={`/${user.slug}`}>
+                            <Avatar
+                                position="fit exact-center"
+                                source={user.avatar || 'http://via.placeholder.com/800?text=Avatar'}
+                                alternate={
+                                    user.name && user.name.first && user.name.last
+                                        ? `${user.name.first} ${user.name.last}`
+                                        : user.name && user.name.first
+                                            ? `${user.name.first}`
+                                            : user.name && user.name.last
+                                                ? `${user.name.last}`
+                                                : user.handle
+                                                    ? user.handle
+                                                    : 'Avatar'
+                                }
+                            />
+                            <h2 className="card-headline name-full">
+                                {user.name && user.name.first && user.name.last
                                     ? `${user.name.first} ${user.name.last}`
                                     : user.name && user.name.first
                                         ? `${user.name.first}`
                                         : user.name && user.name.last
                                             ? `${user.name.last}`
-                                            : user.handle
-                                                ? user.handle
-                                                : 'Avatar'
-                            }
-                        />
-                        <h2 className="card-headline name-full">
-                            {user.name && user.name.first && user.name.last
-                                ? `${user.name.first} ${user.name.last}`
-                                : user.name && user.name.first
-                                    ? `${user.name.first}`
-                                    : user.name && user.name.last
-                                        ? `${user.name.last}`
-                                        : 'Name'}
-                        </h2>
-                        <h3 className="card-tagline handle">@{user.handle || 'handle'}</h3>
-                    </Link>
-                </header>
-            </article>
-        );
-    });
-    return (
-        <main id="main" role="main">
-            <div className="container-fluid">
-                <Basic space="space-xs-30 space-lg-80">
-                    <header className="text-center">
-                        <h1>Users</h1>
+                                            : 'Name'}
+                            </h2>
+                            <h3 className="card-tagline handle">@{user.handle || 'handle'}</h3>
+                        </Link>
                     </header>
-                </Basic>
+                </article>
+            );
+        });
+        return (
+            <main id="main" role="main">
+                <div className="container-fluid">
+                    <Basic space="space-xs-30 space-lg-80">
+                        <header className="text-center">
+                            <h1>Users</h1>
+                        </header>
+                    </Basic>
 
-                <Feed space="space-xs-20 space-lg-80" item={item}>
-                    <section>
-                        {loadingUsers ? (
-                            <Loader position="exact-center fixed" label="Loading users" />
-                        ) : users.length > 0 ? (
-                            <div className="row gutter-20 gutter-lg-80 text-center">{loopUser}</div>
-                        ) : (
-                            <article className="empty text-center">
-                                <header>
-                                    <h3>{`No ${item}s`}</h3>
-                                </header>
-                            </article>
-                        )}
-                    </section>
-                </Feed>
-            </div>
-        </main>
-    );
-};
+                    <Feed space="space-xs-20 space-lg-80" item={item}>
+                        <section>
+                            {loadingUsers ? (
+                                <Loader position="exact-center fixed" label="Loading users" />
+                            ) : users.length > 0 ? (
+                                <div className="row gutter-20 gutter-lg-80 text-center">{loopUser}</div>
+                            ) : (
+                                <article className="empty text-center">
+                                    <header>
+                                        <h3>{`No ${item}s`}</h3>
+                                    </header>
+                                </article>
+                            )}
+                        </section>
+                    </Feed>
+                </div>
+            </main>
+        );
+    }
+}
 
 User.propTypes = {
-    loadingUsers: PropTypes.bool.isRequired,
     users: PropTypes.arrayOf(PropTypes.object).isRequired,
+    actionUser: PropTypes.objectOf(PropTypes.func).isRequired,
 };
 
-function mapStateToProps({ calls, users }) {
+function mapStateToProps({ users }) {
     return {
-        loadingUsers: findByString(calls, removeStatus(USERS_LOAD_REQUEST)),
         users,
     };
 }
 
-export default connect(mapStateToProps)(User);
+function mapDispatchToProps(dispatch) {
+    return {
+        actionUser: bindActionCreators(actionUser, dispatch),
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(User);
