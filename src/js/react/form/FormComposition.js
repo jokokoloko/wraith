e316b7890075016784ad5649d4dc6f0200be98fa
jokokoloko <jhowland82@gmail.com>
2018-07-26 19:46:03 -0700

    Modified layout and styles

--- src/js/react/form/FormComposition.js
+++ src/js/react/form/FormComposition.js
@@ -4,21 +4,17 @@ import InputText from '../input/InputText';
 const FormComposition = ({ formData, onTextChange }) => {
     const size = 'lg';
     return (
-        <form className={`form form-${size} mx-lg-auto`}>
-            <div className="form-row form-gutter-20">
-                <InputText name="title" label="Comp Name" placeholder="Comp Name" value={formData.title} size={size} onChange={onTextChange} />
-            </div>
-            <div className="form-row form-gutter-20">
-                <InputText
-                    type="area"
-                    name="description"
-                    label="description"
-                    placeholder="How does this comp win?"
-                    value={formData.description}
-                    size={size}
-                    onChange={onTextChange}
-                />
-            </div>
+        <form id="form-composition" className={`form form-${size} mx-lg-auto`}>
+            <InputText name="title" label="Comp Name" placeholder="Comp Name" value={formData.title} size={size} onChange={onTextChange} />
+            <InputText
+                type="area"
+                name="description"
+                label="description"
+                placeholder="How does this comp win?"
+                value={formData.description}
+                size={size}
+                onChange={onTextChange}
+            />
         </form>
     );
 };
