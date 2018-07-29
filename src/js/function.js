be3ef4d621d5be71225e2b715162a170119fecf7
jokokoloko <jhowland82@gmail.com>
2018-07-28 20:46:49 -0700

    Modified slugify() and excerptify() in function.js

--- src/js/function.js
+++ src/js/function.js
@@ -26,31 +26,33 @@ export const slugify = (text) => {
     const b = 'aaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------';
     const p = new RegExp(a.split('').join('|'), 'g');
     return (
-        text
-            .toString()
-            .toLowerCase()
-            // Replace spaces with -
-            .replace(/\s+/g, '-')
-            // Replace special chars
-            .replace(p, (c) => b.charAt(a.indexOf(c)))
-            // Replace & with 'and'
-            .replace(/&/g, '-and-')
-            // Remove all non-word chars
-            .replace(/[^\w-]+/g, '')
-            // Replace multiple - with single -
-            .replace(/--+/g, '-')
-            // Replace something with single -
-            .replace(/[\s_-]+/g, '-')
-            // Trim - from start of text
-            .replace(/^-+/, '')
-            // Trim - from end of text
-            .replace(/-+$/, '')
-            // Trim spaces from start and end of text
-            .trim()
+        (text &&
+            text
+                .toString()
+                .toLowerCase()
+                // Replace spaces with -
+                .replace(/\s+/g, '-')
+                // Replace special chars
+                .replace(p, (c) => b.charAt(a.indexOf(c)))
+                // Replace & with 'and'
+                .replace(/&/g, '-and-')
+                // Remove all non-word chars
+                .replace(/[^\w-]+/g, '')
+                // Replace multiple - with single -
+                .replace(/--+/g, '-')
+                // Replace something with single -
+                .replace(/[\s_-]+/g, '-')
+                // Trim - from start of text
+                .replace(/^-+/, '')
+                // Trim - from end of text
+                .replace(/-+$/, '')
+                // Trim spaces from start and end of text
+                .trim()) ||
+        null
     );
 };
 
-export const excerptify = (text, limit) => (text.length > limit ? text.substr(0, text.lastIndexOf(' ', limit)) : text);
+export const excerptify = (text, limit) => (text && (text.length > limit ? text.substr(0, text.lastIndexOf(' ', limit)) : text)) || null;
 
 // Array
 export const arrayToObject = (array, key) =>
