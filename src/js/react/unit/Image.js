1f8f05db61cac7ebf6155288d7a2b40764bd8221
jokokoloko <jhowland82@gmail.com>
2018-08-12 16:54:43 -0700

    Refactored Image.js into a "dumb" component

--- src/js/react/unit/Image.js
+++ src/js/react/unit/Image.js
@@ -1,58 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import Loader from './Loader';
 
-class Image extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loaded: false,
-            error: false,
-        };
-    }
-    componentDidMount() {
-        const image = document.createElement('img');
-        image.src = this.props.source;
-        image.onload = () => {
-            this.setState({
-                loaded: true,
-                error: false,
-            });
-            this.props.onLoad && this.props.onLoad(image);
-        };
-        image.onerror = (error) => {
-            this.setState({
-                loaded: false,
-                error: true,
-            });
-            this.props.onError && this.props.onError(error);
-        };
-    }
-    render() {
-        const { position, source, alternate } = this.props;
-        const { loaded, error } = this.state;
-        return loaded === false ? (
-            <Loader position="exact-center" error={error} />
-        ) : (
-            <img className={`img-fluid ${position}`} src={source} alt={alternate} />
-        );
-    }
-}
+const Image = ({ className, position, source, alternate }) => <img className={`${className} ${position} img-fluid`} src={source} alt={alternate} />;
 
 Image.propTypes = {
+    className: PropTypes.string,
     position: PropTypes.string,
     source: PropTypes.string,
     alternate: PropTypes.string,
-    onLoad: PropTypes.func,
-    onError: PropTypes.func,
 };
 
 Image.defaultProps = {
+    className: 'no-class',
     position: 'no-position',
     source: undefined,
     alternate: undefined,
-    onLoad: undefined,
-    onError: undefined,
 };
 
 export default Image;
