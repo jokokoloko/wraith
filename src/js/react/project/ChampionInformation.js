5e3be96ead16bda4547c64b4337f568d1665d5e9
Dominique Canlas <canlasdominique@gmail.com>
2018-08-19 12:57:49 -0700

    champ info plus initial work on meta stuff

--- src/js/react/project/ChampionInformation.js
+++ src/js/react/project/ChampionInformation.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as client from '../../client';
+import Image from '../unit/Image';
 
-const ChampionInformation = ({ champion }) => <h1 className="p-xs-15">{champion.name || 'Select a champion.'}</h1>;
+const ChampionInformation = ({ champion }) => {
+    let champImg,
+        roles = '';
+    if (champion && champion.name) {
+        console.log('my champ', champion);
+        champImg = client.CHAMPION_LOADING + champion.key + '_0.jpg';
+        Object.keys(champion.tags).forEach((tag) => {
+            roles += tag + ', ';
+        });
+        //remove the extra comma.
+        roles = roles.substring(0, roles.length - 2);
+    }
+    return champion && champion.name ? (
+        <div className="champion-info">
+            <div className="img-container">
+                <Image source={champImg} alternate={champion.name} />
+            </div>
+            <h1 className="p-xs-15">{champion.name}</h1>
+            <h2 className="p-xs-15">{champion.title}</h2>
+            <h4 className="p-xs-15">Roles: {roles}</h4>
+        </div>
+    ) : (
+        <h1 className="p-xs-15">Select a Champion.</h1>
+    );
+};
 
 ChampionInformation.propTypes = {
     champion: PropTypes.objectOf(PropTypes.any).isRequired,
--- src/js/react/project/CompositionMeta.js
+++ src/js/react/project/CompositionMeta.js
@@ -5,17 +5,47 @@ import InputText from '../input/InputText';
 const CompositionMeta = ({ form, onChange }) => {
     const size = 'lg';
     return (
-        <form id="form-composition" className={`form form-${size} panel`}>
-            <InputText name="title" label="Title" placeholder="Title" size={size} onChange={onChange} value={form.title} />
-            <InputText
-                type="area"
-                name="description"
-                label="Description"
-                placeholder="Description"
-                size={size}
-                onChange={onChange}
-                value={form.description}
-            />
+        <form id="form-composition" className={`form form-${size}`}>
+            <div className="panel mt-5">
+                <InputText
+                    name="title"
+                    label="Title"
+                    placeholder="Title"
+                    size={size}
+                    onChange={onChange}
+                    value={form.title}
+                />
+                <InputText
+                    type="area"
+                    name="description"
+                    label="Description"
+                    placeholder="Description"
+                    size={size}
+                    onChange={onChange}
+                    value={form.description}
+                />
+            </div>
+
+            <div className="panel mt-5">
+                <InputText
+                    type="area"
+                    name="description"
+                    label="Description"
+                    placeholder="Description"
+                    size={size}
+                    onChange={onChange}
+                    value={form.foo}
+                />
+                <InputText
+                    type="area"
+                    name="description"
+                    label="Description"
+                    placeholder="Description"
+                    size={size}
+                    onChange={onChange}
+                    value={form.bar}
+                />
+            </div>
         </form>
     );
 };
